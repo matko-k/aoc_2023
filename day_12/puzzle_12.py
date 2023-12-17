22ff58ef8832939eefbd22634cd78f1f7bd3887d
matko <incubed@ThinkPad-P14s.localdomain>
2023-12-17 20:06:56 +0100

    Add day_12

--- day_12/puzzle_12.py
+++ day_12/puzzle_12.py
@@ -1,29 +1,62 @@
 
-def is_spring_valid(spring, groups):
-    s = ''
+def state_to_str(springs, groups):
+    res = ''
+    for s in springs:
+        res += s + '|'
+    for num in groups:
+        res += str(num) + '|'
 
-    for c in spring:
-        if c != '.' or not s or c != s[-1]:
-            s += c
-
-    sections = s.strip('.').split('.')
-    if len(sections) != len(groups):
-        return False
-
-    for i in range(len(sections)):
-        if len(sections[i]) != groups[i]:
-            return False
-
-    return True
+    return res
 
 
-def find_combinations(spring, groups):
-    if '?' not in spring:
-        return 1 if is_spring_valid(spring, groups) else 0
+def find_combinations(springs, groups, state_map):
 
-    i = spring.find('?')
-    return (find_combinations(spring[:i] + '.' + spring[i+1:], groups)
-            + find_combinations(spring[:i] + '#' + spring[i+1:], groups))
+    state_id = state_to_str(springs, groups)
+    if state_id in state_map.keys():
+        return state_map[state_id]
+
+    if len(springs) == 0:
+        if len(groups) == 0:
+            return 1
+        return 0
+
+    if len(groups) == 0:
+        for sp in springs:
+            if '#' in sp:
+                return 0
+        return 1
+
+    next_springs = springs[0]
+    next_group = groups[0]
+
+    if len(next_springs) == next_group:
+        if '#' in next_springs:
+            return find_combinations(springs[1:], groups[1:], state_map)
+
+    if len(next_springs) < next_group:
+        if '#' in next_springs:
+            return 0
+        return find_combinations(springs[1:], groups, state_map)
+
+    res = 0
+    i = next_springs.find('?')
+    if i >= 0:
+        next_1 = next_springs[:i]
+        next_2 = next_springs[i+1:]
+        if len(next_1) > 0 and len(next_2) > 0:
+            res += find_combinations([next_1] + [next_2] + springs[1:], groups, state_map)
+        elif len(next_2) == 0 and len(next_1) > 0:
+            res += find_combinations([next_1] + springs[1:], groups, state_map)
+        elif len(next_1) == 0 and len(next_2) > 0:
+            res += find_combinations([next_2] + springs[1:], groups, state_map)
+        else:
+            res += find_combinations(springs[1:], groups, state_map)
+
+        next = next_1 + '#' + next_2
+        res += find_combinations([next] + springs[1:], groups, state_map)
+
+    state_map[state_id] = res
+    return res
 
 
 def parse_input(filename, part2=False):
@@ -41,30 +74,30 @@ def parse_input(filename, part2=False):
                     total_nums.extend(nums)
                 spring = total_spring
                 nums = total_nums
-            springs.append(spring)
+            springs.append([x for x in spring.strip('.').split('.') if x])
             groups.append(nums)
 
     return springs, groups
 
 
 def part_1():
-    springs, groups = parse_input('inputs/example_12.txt')
+    springs, groups = parse_input('inputs/my_input_12.txt')
     sum = 0
-
     for i in range(len(springs)):
-        sum += find_combinations(springs[i], groups[i])
+        state_map = {}
+        sum += find_combinations(springs[i], groups[i], state_map)
 
     return sum
 
 
 def part_2():
-    springs, groups = parse_input('inputs/example_12.txt', True)
+    springs, groups = parse_input('inputs/my_input_12.txt', True)
     sum = 0
+    for i in range(len(springs)):
+        state_map = {}
+        sum += find_combinations(springs[i], groups[i], state_map)
 
-    # for i in range(len(springs)):
-    #     sum += find_combinations(springs[i], groups[i])
-    #
-    # return sum
+    return sum
 
 
 if __name__ == "__main__":
